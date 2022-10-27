b9fcbae8bb5ad3d4a97020425b905f871754d008
Ogravno <odin@grav.name>
2022-10-27 18:18:01 +0200

    added a dislike buttojn and an input field

--- like_button.js
+++ like_button.js
@@ -19,8 +19,58 @@ class LikeButton extends React.Component {
   }
 }
 
+class DislikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { disliked: false };
+  }
+
+  render() {
+    if (this.state.disliked) {
+      return 'You disliked this.';
+    }
+
+    return (
+      <button onClick = {() => this.setState({ disliked: true }) }>
+        Dislike
+      </button>
+    );
+  }
+}
+
+class CommentInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { commented: false };
+  }
+
+  render() {
+    if (this.state.commented) {
+      return 'You commented on this.';
+    }
+
+    console.log(this)
+    return (
+      <React.Fragment>
+        <input placeholder = "comment"></input>
+        <button onClick = {() => this.setState({ commented: true }) }>
+          comment
+        </button>
+      </React.Fragment>
+    );
+  }
+}
+
 const e = React.createElement;
 
-const domContainer2 = document.querySelector('#comment_input_container');
-const root2 = ReactDOM.createRoot(domContainer2);
-root2.render(e(LikeButton));
+const domContainerLikeButton = document.querySelector('#like_button_container');
+const rootLikeButton = ReactDOM.createRoot(domContainerLikeButton);
+rootLikeButton.render(e(LikeButton));
+
+const domContainerDislikeButton = document.querySelector('#dislike_button_container');
+const rootDislikeButton = ReactDOM.createRoot(domContainerDislikeButton);
+rootDislikeButton.render(e(DislikeButton));
+
+const domContainerCommentInput = document.querySelector('#comment_input_container');
+const rootCommentInput = ReactDOM.createRoot(domContainerCommentInput);
+rootCommentInput.render(e(CommentInput));
