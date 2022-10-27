81425eebe44938871043426799a68e32026b6696
Ogravno <odin@grav.name>
2022-10-27 14:46:19 +0200

    Added a dislike button

--- like_button.js
+++ like_button.js
@@ -21,6 +21,29 @@ class LikeButton extends React.Component {
   }
 }
 
+class DislikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { disliked: false };
+  }
+
+  render() {
+    if (this.state.disliked) {
+      return 'You disliked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ disliked: true }) },
+      'Dislike'
+    );
+  }
+}
+
 const domContainer = document.querySelector('#like_button_container');
 const root = ReactDOM.createRoot(domContainer);
 root.render(e(LikeButton));
+
+const domContainer1 = document.querySelector('#dislike_button_container');
+const root1 = ReactDOM.createRoot(domContainer1);
+root1.render(e(DislikeButton));
